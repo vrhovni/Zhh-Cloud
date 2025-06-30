8dd418ccfebd37bbbb5941548de5d08ba6c651d8
vrhovni <1320497010@qq.com>
2025-06-30 11:32:38 +0800

    微服务

--- src/main/java/com/zhh/clouddemo/CloudDemoApplication.java
+++ src/main/java/com/zhh/clouddemo/CloudDemoApplication.java
@@ -2,12 +2,13 @@ package com.zhh.clouddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CloudDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudDemoApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(CloudDemoApplication.class, args);
     }
 
 }
