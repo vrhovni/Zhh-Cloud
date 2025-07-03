585302f30f7d691c710c33921b9816fc0cf2de8c
vrhovni <1320497010@qq.com>
2025-07-03 22:45:08 +0800

    微服务

--- services/services-order/src/main/java/com/zhh/order/controller/OrderController.java
+++ services/services-order/src/main/java/com/zhh/order/controller/OrderController.java
@@ -3,21 +3,32 @@ package com.zhh.order.controller;
 import com.zhh.order.bean.Order;
 import com.zhh.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope// 开启自动刷新
 @RestController
 public class OrderController {
 
     @Autowired
     OrderService orderService;
 
+    @Value("${order.timeout}")
+    String orderTimeout;
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+
+    @GetMapping("/config")
+    public String config(){
+        return "orderTimeout:"+orderTimeout+",orderAutoConfirm:"+orderAutoConfirm;
+    }
+
     @GetMapping("/create")
     public Order createOrder(@RequestParam("userId") Long userId, @RequestParam("productId") Long productId) {
         Order order = orderService.createOrder(productId, userId);
         return order;
     }
-
-
 }
